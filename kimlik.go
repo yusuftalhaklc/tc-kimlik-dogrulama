cc6903b74ed14f787a83f13a6cd783a3d580a4f2
yusuf_talha_kilic <yusuf.talha.kilic@intern.priviasecurity.com>
2023-09-30 14:19:23 +0300

    Add error handling

--- kimlik.go
+++ kimlik.go
@@ -3,7 +3,6 @@ package tckimlik
 import (
 	"bytes"
 	"encoding/xml"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 
@@ -11,7 +10,7 @@ import (
 	"github.com/yusuftalhaklc/tc-kimlik-dogrulama/util"
 )
 
-func Dogrula(tc *model.TCKimlik) bool {
+func Dogrula(tc *model.TCKimlik) (bool, error) {
 	payload := model.Envelope{
 		XSI:    util.XSI,
 		XSD:    util.XSD,
@@ -22,41 +21,36 @@ func Dogrula(tc *model.TCKimlik) bool {
 	}
 	xmlPayload, err := xml.Marshal(payload)
 	if err != nil {
-		fmt.Println("Error marshaling XML:", err)
-		return false
+		return false, err
 	}
 	return sendHTTPClient(xmlPayload)
 }
 
-func sendHTTPClient(xmlPayload []byte) bool {
+func sendHTTPClient(xmlPayload []byte) (bool, error) {
 	payload := bytes.NewReader(xmlPayload)
 	client := &http.Client{}
-	req, err := http.NewRequest(util.HTTP_METHOD, util.BASE_URL, payload)
 
+	req, err := http.NewRequest(util.HTTP_METHOD, util.BASE_URL, payload)
 	if err != nil {
-		fmt.Println(err)
-		return false
+		return false, err
 	}
 	req.Header.Add("Content-Type", util.CONTENT_TYPE)
 
 	res, err := client.Do(req)
 	if err != nil {
-		fmt.Println(err)
-		return false
+		return false, err
 	}
 	defer res.Body.Close()
 
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		fmt.Println(err)
-		return false
+		return false, err
 	}
 
 	response := new(model.TCKimlikNoDogrulaResponse)
 	if err := xml.Unmarshal([]byte(body), &response); err != nil {
-		fmt.Println("Error:", err)
-		return false
+		return false, err
 	}
 
-	return response.Body.Result.Value
+	return response.Body.Result.Value, nil
 }
